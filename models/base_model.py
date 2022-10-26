1b319c91efe9f57cf27624cab461786be3ff8bf4
AnActualBanana <loganmcclain59@gmail.com>
2022-10-26 12:31:08 -0700

    Revert "New Storage engine"
    This reverts commit 4fe848b33269c664ac97830cd619cf99131730f8.

--- models/base_model.py
+++ models/base_model.py
@@ -1,64 +1,44 @@
 #!/usr/bin/python3
-"""A Base class for all models."""
+"""This module defines a base class for all models in our hbnb clone"""
 import uuid
 from datetime import datetime
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String, DateTime
-import models
-
-
-Base = declarative_base()
 
 
 class BaseModel:
-    """A base class for all models"""
-    id = Column(String(60), primary_key=True, nullable=False)
-    created_at = Column(DateTime, default=datetime.utcnow(), nullable=False)
-    updated_at = Column(DateTime, default=datetime.utcnow(), nullable=False)
-
+    """A base class for all hbnb models"""
     def __init__(self, *args, **kwargs):
-        """Instantiates a new model"""
-        if id not in kwargs:
+        """Instatntiates a new model"""
+        if not kwargs:
+            from models import storage
             self.id = str(uuid.uuid4())
-        if "created_at" not in kwargs:
             self.created_at = datetime.now()
-        else:
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-
-        if "updated_at" not in kwargs:
             self.updated_at = datetime.now()
+            storage.new(self)
         else:
             kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
                                                      '%Y-%m-%dT%H:%M:%S.%f')
-        if "__class__" in kwargs:
+            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
+                                                     '%Y-%m-%dT%H:%M:%S.%f')
             del kwargs['__class__']
-
-        self.__dict__.update(kwargs)
+            self.__dict__.update(kwargs)
 
     def __str__(self):
-        """Returns a string representation of the object"""
+        """Returns a string representation of the instance"""
         cls = (str(type(self)).split('.')[-1]).split('\'')[0]
         return '[{}] ({}) {}'.format(cls, self.id, self.__dict__)
 
     def save(self):
-        """Updates updated_at with current time"""
+        """Updates updated_at with current time when instance is changed"""
+        from models import storage
         self.updated_at = datetime.now()
-        models.storage.new(self)
-        models.storage.save()
+        storage.save()
 
     def to_dict(self):
-        """Converts object into a dictionary"""
+        """Convert instance into dict format"""
         dictionary = {}
         dictionary.update(self.__dict__)
         dictionary.update({'__class__':
                           (str(type(self)).split('.')[-1]).split('\'')[0]})
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
-        if '_sa_instance_state' in dictionary.keys():
-            dictionary.pop('_sa_instance_state', None)
         return dictionary
-
-    def delete(self):
-        """Deletes instance"""
-        models.storage.delete(self)
--- models/city.py
+++ models/city.py
@@ -1,16 +1,9 @@
 #!/usr/bin/python3
-"""City Module"""
-from models.base_model import BaseModel, Base
-from models.place import Place
-from sqlalchemy import Column, String, ForeignKey
-from sqlalchemy.orm import relationship
+""" City Module for HBNB project """
+from models.base_model import BaseModel
 
 
-class City(BaseModel, Base):
-    """the city class"""
-    state_id = Column(String(90), ForeignKey('states.id'), nullable=False)
-    name = Column(String(200), nullable=False)
-    __tablename__ = 'cities'
-
-    places = relationship('Place', backref='cities',
-                          cascade='all, delete, delete-orphan')
+class City(BaseModel):
+    """ The city class, contains state ID and name """
+    state_id = ""
+    name = ""
